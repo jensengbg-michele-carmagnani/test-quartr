8d22130488a4f945d180e6df5f72b174c92ecf4f
Michele Carmigani <michelecarmigani@Viktors-MacBook-Pro-2.local>
2023-01-02 09:39:20 +0100

    css in layout

--- app/(user)/companies/[id]/layout.tsx
+++ app/(user)/companies/[id]/layout.tsx
@@ -8,11 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <main className="flex ">
-      <div className="pl-10 overflow-auto">
+      <div className="pl-10 min-w-[50%] overflow-auto">
         {/* @ts-ignore */}
         <TrendCompaniesList />
       </div>
-      <div className="flex-1 overflow-hidden fixed top-[10%] left-[40%]">
+      <div className="flex-1 overflow-hidden fixed top-[10%] left-[50%]">
         {children}
       </div>
     </main>
